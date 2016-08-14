0a302895081e93b30047a3e140b691f184148c5a
BethJSmith <bethanys1609@gmail.com>
2016-08-14 17:29:26 +1000

    First Android work
    Completed first Android app using online tutorials from Xamarin. It's a
    password encrypter. Typing in a word will show you an encrypted version.
    There is also an option to unencrypt a word.

---
+++ Android Work/PasswordEncryption/AndroidApp/Encryption.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace AndroidApp
+{
+	public static class Encryption
+	{
+		
+		public static string Encrypt(string input)
+		{
+			char[] values1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".ToCharArray();
+			char[] values2 = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+			string output = "";
+
+			foreach (char c in input)
+			{
+				int i = 0;
+				bool found = false;
+				foreach (char ch in values1)
+				{
+					if (c == ch)
+					{
+						found = true;
+					}
+					else if (found == false)
+						i++;
+				}
+				if (found == false)
+					output = output + c;
+				else
+					output = output + values2[i];
+
+			}
+			return output;
+		}
+
+		public static string Decrypt(string input)
+		{
+			char[] values1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".ToCharArray();
+			char[] values2 = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+			string output = "";
+
+			foreach (char c in input)
+			{
+				int i = 0;
+				bool found = false;
+				foreach (char ch in values2)
+				{
+					if (c == ch)
+					{
+						found = true;
+					}
+					else if (found == false)
+						i++;
+				}
+				if (found == false)
+					output = output + c;
+				else
+					output = output + values1[i];
+
+			}
+
+			return output;
+		}
+	}
+}
+
---
+++ Android Work/PasswordEncryption/AndroidApp/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace AndroidApp
+{
+	[Activity(Label = "Android App", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get all resources on screen
+			EditText plainText = FindViewById<EditText>(Resource.Id.PlainText);
+			EditText codeText = FindViewById<EditText>(Resource.Id.CodeText);
+			TextView encryptedText = FindViewById<TextView>(Resource.Id.EncryptedText);
+			TextView decryptedText = FindViewById<TextView>(Resource.Id.DecryptedText);
+			Button encryptButton = FindViewById<Button>(Resource.Id.EncryptButton);
+			Button decryptButton = FindViewById<Button>(Resource.Id.DecryptButton);
+
+			// Encrypt text
+			encryptButton.Click += (object sender, EventArgs e) =>
+			{
+				encryptedText.Text = Encryption.Encrypt(plainText.Text);
+			};
+
+			// Decrypt text
+			decryptButton.Click += (object sender, EventArgs e) =>
+			{
+				decryptedText.Text = Encryption.Decrypt(codeText.Text);
+			};
+		}
+	}
+}
+
+
