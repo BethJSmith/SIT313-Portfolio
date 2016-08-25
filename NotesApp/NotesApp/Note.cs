a328cc1fce8b92f92218ee76857bc0cc2d5c636f
BethJSmith <bethany@Bethanys-MacBook-Air.local>
2016-08-25 16:03:11 +1000

    Created NotesApp
    NoteApp will be an initial test of using Xamarin.Forms to create a
    cross-platform app.

---
+++ NotesApp/NotesApp/Note.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace NotesApp
+{
+	public class Note
+	{
+		private string _Message;
+		private bool _Completed;
+
+		public string Message
+		{
+			get
+			{
+				return _Message;
+			}
+		}
+
+		public bool Completed
+		{
+			get
+			{
+				return _Completed;
+			}
+		}
+
+		// Constructor that takes a single string, all notes will be incomplete by default
+		public Note(string message)
+		{
+			_Message = message;
+			_Completed = false;
+		}
+
+		// Updates the message for the note
+		public void Update(string newMessage)
+		{
+			_Message = newMessage;
+		}
+
+		// Changes the state of the note between completed and not completed
+		public void Check()
+		{
+			if (_Completed)
+				_Completed = false;
+			else
+				_Completed = true;
+		}
+	}
+}
---
+++ NotesApp/NotesApp/NotesList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic:
+
+namespace NotesApp
+{
+	public class NotesList
+	{
+		List<Note> List;
+
+		public NotesList()
+		{
+			List = new List<Note>()
+
+		}
+	}
+}
+
