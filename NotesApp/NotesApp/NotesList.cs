46fb1f3d549854e5027e3f061e680f3cebff2801
BethJSmith <bethanys1609@gmail.com>
2016-08-25 16:21:11 +1000

    Outlined NotesList class
    Created the base for the NotesList class. All methods defined and still
    need to be completed.

--- NotesApp/NotesApp/NotesList.cs
+++ NotesApp/NotesApp/NotesList.cs
@@ -7,10 +7,47 @@ namespace NotesApp
 	{
 		List<Note> List;
 
-		public NotesList()
-		{
-			List = new List<Note>()
+		private string _Filename;
 
+		// Constructor requires filename for location of stored messages, will be different in different OSs
+		public NotesList(string filename)
+		{
+			List = new List<Note>();
+
+			_Filename = filename;
+
+			LoadList();
+
+		}
+
+		// Add a new note to the list and save the file
+		public void AddNote(string message)
+		{
+			// Code here...
+
+
+			SaveList();
+		}
+
+		// Edit an existing note and save the file
+		public void EditNote()
+		{
+			// Code here...
+
+
+			SaveList();
+		}
+
+		// Save the list to the file, overwriting all previous data
+		private void SaveList()
+		{
+			// Code here...
+		}
+
+		// Load the list from the file
+		private void LoadList()
+		{
+			// Code here...
 		}
 	}
 }
