e147eae05af2cf359c47b68fdadf814c31a421c1
BethJSmith <bethanys1609@gmail.com>
2016-10-02 17:56:54 +1100

    Create API session app

---
+++ ConferenceSessionsAPIs/ConferenceSessionsAPIs/Controllers/ConferenceSessionsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+
+namespace ConferenceSessionsAPIs.Controllers
+{
+	public class ConferenceSessionsController : ApiController
+    {
+
+		List<Session> sessions = new List<Session>(
+			new Session[] {
+				new Session { Title = "Microsoft", Description = "Azure!" },
+				new Session { Title = "Google", Description = "Android!" },
+				new Session { Title = "Facebook", Description = "What's App!" },
+				new Session { Title = "IBM", Description = "Watson!" }
+			}
+		);
+
+		public List<Session> GetAll()
+		{
+			return sessions;
+		}
+
+		public Session GetSession(string id)
+		{
+			var session = sessions.FirstOrDefault((p) => p.Title == id);
+			return session;
+		}
+    }
+}
---
+++ ConferenceSessionsAPIs/ConferenceSessionsAPIs/Models/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ConferenceSessionsAPIs
+{
+	public class Session
+	{
+		public string Title;
+		public string Description;
+
+		public Session()
+		{
+		}
+	}
+}
+
